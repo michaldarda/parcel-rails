94643d37cd82ce5234246ec7609c33659468bd65
Michał Darda <michaldarda@gmail.com>
2018-02-05 22:13:22 +0100

    Initial commit

---
+++ lib/parcel/rails.rb
@@ -0,0 +1,7 @@
+require "parcel/rails/version"
+
+module Parcel
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ spec/parcel/rails_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Parcel::Rails do
+  it "has a version number" do
+    expect(Parcel::Rails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
