46c604506e7b9e2bf0249148f058ea5e0133f96d
Michał Darda <michaldarda@gmail.com>
2018-02-10 17:59:57 +0100

    First working version

--- lib/parcel-rails.rb
+++ lib/parcel-rails.rb
@@ -2,6 +2,7 @@
 
 require 'parcel/rails/version'
 require 'parcel/rails/runner'
+require 'parcel/rails/view_helpers'
 
 module Parcel
   # Parceljs integration with Rails
@@ -15,6 +16,10 @@ module Parcel
 
       class Railtie < ::Rails::Railtie
         railtie_name :parcel
+
+        initializer 'parcel.view_helpers' do
+          ActionView::Base.send :include, ViewHelpers
+        end
 
         rake_tasks do
           namespace :parcel do
